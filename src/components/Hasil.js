c6324a48105137c0430475ea0626f34473f4638e
DavinaCahyani <davinacahyaniputri@gmail.com>
2024-02-24 11:45:12 +0700

    update dan delete

--- src/components/Hasil.js
+++ src/components/Hasil.js
@@ -2,8 +2,112 @@ import React, { Component } from "react";
 import { Badge, Col, ListGroup, Row } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
 import TotalBayar from "./TotalBayar";
+import ModalKeranjang from "./ModalKeranjang";
+import axios from "axios";
+import { API_URL } from "../utils/constants";
+import swal from "sweetalert";
 
 export default class Hasil extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false,
+      keranjangDetail: false,
+      jumlah: 0,
+      keterangan: "",
+      totalHarga: 0,
+    };
+  }
+
+  handleShow = (menuKeranjang) => {
+    this.setState({
+      showModal: true,
+      keranjangDetail: menuKeranjang,
+      jumlah: menuKeranjang.jumlah,
+      keterangan: menuKeranjang.keterangan,
+      totalHarga: menuKeranjang.total_harga,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      showModal: false,
+    });
+  };
+
+  tambah = () => {
+    this.setState({
+      jumlah: this.state.jumlah + 1,
+      totalHarga:
+        this.state.keranjangDetail.product.harga * (this.state.jumlah + 1),
+    });
+  };
+
+  kurang = () => {
+    if (this.state.jumlah !== 1) {
+      this.setState({
+        jumlah: this.state.jumlah - 1,
+        totalHarga:
+          this.state.keranjangDetail.product.harga * (this.state.jumlah - 1),
+      });
+    }
+  };
+
+  changeHandler = (event) => {
+    this.setState({
+      keterangan: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.handleClose();
+
+    const data = {
+      jumlah: this.state.jumlah,
+      total_harga: this.state.totalHarga,
+      product: this.state.keranjangDetail.product,
+      keterangan: this.state.keterangan,
+    };
+
+    axios
+      .put(API_URL + "keranjangs/" + this.state.keranjangDetail.id, data)
+      .then((res) => {
+        swal({
+          title: "Update Pesanan!",
+          text: "Sukses Update Pesanan " + data.product.nama,
+          icon: "success",
+          button: false,
+          timer: 1500,
+        });
+      })
+      .catch((error) => {
+        console.log("Error : ", error);
+      });
+  };
+
+  hapusPesanan = (id) => {
+    this.handleClose();
+
+    axios
+      .delete(API_URL + "keranjangs/" + id)
+      .then((res) => {
+        swal({
+          title: "Hapus Pesanan!",
+          text:
+            "Sukses Hapus Pesanan " + this.state.keranjangDetail.product.nama,
+          icon: "error",
+          button: false,
+          timer: 1500,
+        });
+      })
+      .catch((error) => {
+        console.log("Error : ", error);
+      });
+  };
+
   render() {
     const { keranjangs } = this.props;
     return (
@@ -15,7 +119,10 @@ export default class Hasil extends Component {
         {keranjangs.length !== 0 && (
           <ListGroup variant="flush">
             {keranjangs.map((menuKeranjang) => (
-              <ListGroup.Item key={menuKeranjang}>
+              <ListGroup.Item
+                key={menuKeranjang}
+                onClick={() => this.handleShow(menuKeranjang)}
+              >
                 <Row>
                   <Col xs={2}>
                     <h4>
@@ -36,6 +143,16 @@ export default class Hasil extends Component {
                 </Row>
               </ListGroup.Item>
             ))}
+
+            <ModalKeranjang
+              handleClose={this.handleClose}
+              {...this.state}
+              tambah={this.tambah}
+              kurang={this.kurang}
+              changeHandler={this.changeHandler}
+              handleSubmit={this.handleSubmit}
+              hapusPesanan={this.hapusPesanan}
+            />
           </ListGroup>
         )}
         <TotalBayar keranjangs={keranjangs} {...this.props} />
